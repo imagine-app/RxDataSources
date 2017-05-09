0ab4e341a0ff788901bc91c1bafe43425fcefb5a
Aurélien Noce <aurnoce@gmail.com>
2017-05-09 10:28:03 +0200

    Fix initial load issue on iPhone5s

--- Sources/DataSources/TableViewSectionedDataSource.swift
+++ Sources/DataSources/TableViewSectionedDataSource.swift
@@ -233,6 +233,9 @@ open class TableViewSectionedDataSource<S: SectionModelType>
     }
     
     open override func _rx_tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if _sectionModels.count <= section {
+            return 0
+        }
         return _sectionModels[section].items.count
     }
     
